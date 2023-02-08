7445d318de7c4358d288305e6ac58c30f817cfa9
ronitshinde <rony.shinde753@gmail.com>
2023-02-08 09:05:30 +0530

    [Ronit] Add : UC1-Flip Coins

---
+++ BasicsCorePrograms/BasicsCorePrograms/FlipCoins.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BasicsCorePrograms
+{
+    public class FlipCoins
+    {
+        int countHead = 0;
+        int countTail = 0;
+        public void Coin()
+        {
+            Console.WriteLine("Enter the number of times to flip the coin");
+            int flipNum = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+            for (int i = 0; i < flipNum; i++)
+            {
+                double numCheck = random.NextDouble();
+                if (numCheck > 0.5)
+                    countHead++;
+                else
+                    countTail++;
+            }
+            Console.WriteLine("Count of Head : " + countHead);
+            Console.WriteLine("Count of Tail : " + countTail);
+
+            float headPercentage = (float)countHead / flipNum * 100;
+            float tailPercentage = (float)countTail / flipNum * 100;
+
+            Console.WriteLine("Percentage of head : " + headPercentage);
+            Console.WriteLine("Percentage of tail : " + tailPercentage);
+        }
+    }
+}
+   
---
+++ BasicsCorePrograms/BasicsCorePrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicsCorePrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("1.FlipCoin");
+            Console.WriteLine("Enter a option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoins flip = new FlipCoins();
+                    flip.Coin();
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
