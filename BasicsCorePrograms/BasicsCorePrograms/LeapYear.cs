4e12bceac961fa88a02af15bf3c67d6114eae4e3
ronitshinde <rony.shinde753@gmail.com>
2023-02-08 14:01:14 +0530

    [Ronit] Add : UC2-Leap Year

---
+++ BasicsCorePrograms/BasicsCorePrograms/LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicsCorePrograms
+{
+    public class LeapYear
+    {
+        public void Leap()
+        {
+            Console.WriteLine("Enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 == 0) || year % 100 == 0)
+                Console.WriteLine("It is a leap year");
+            else
+                Console.WriteLine("It is not a leap year");
+
+        }
+
+    }
+}
+
+    
--- BasicsCorePrograms/BasicsCorePrograms/Program.cs
+++ BasicsCorePrograms/BasicsCorePrograms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace BasicsCorePrograms
 {
@@ -6,7 +7,7 @@ namespace BasicsCorePrograms
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("1.FlipCoin");
+            Console.WriteLine("1.FlipCoin \n2.LeapYear ");
             Console.WriteLine("Enter a option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -14,6 +15,11 @@ namespace BasicsCorePrograms
                 case 1:
                     FlipCoins flip = new FlipCoins();
                     flip.Coin();
+                    Console.ReadLine();
+                    break;
+                case 2:
+                    LeapYear year = new LeapYear();
+                    year.Leap();
                     Console.ReadLine();
                     break;
             }
