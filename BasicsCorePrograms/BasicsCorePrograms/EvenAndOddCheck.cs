8e495d6afe82989ea8ce16e33e2002eb23355793
ronitshinde <118701033+ronitshinde@users.noreply.github.com>
2023-02-09 23:12:31 +0530

    [Ronit] Add : UC7-Even And Odd Check

--- BasicsCorePrograms/BasicsCorePrograms/EvenAndOddCheck.cs
+++ BasicsCorePrograms/BasicsCorePrograms/EvenAndOddCheck.cs
@@ -6,19 +6,19 @@ namespace BasicsCorePrograms
     {
         public void EvenOdd()
         {
-            int a, b = 2, c ;
+            int numOne, numTwo = 2, num ;
 
             Console.WriteLine("Enter the Number");
-            a = Convert.ToInt32(Console.ReadLine());
-            c = a % b;
+            numOne = Convert.ToInt32(Console.ReadLine());
+            num = numOne % numTwo;
 
-            if (c == 0)
+            if (num == 0)
             {
-                Console.WriteLine("Number is Even : " + a);
+                Console.WriteLine("Number is Even : " + numOne);
             }
             else
             {
-                Console.WriteLine("Number is odd : " + a);
+                Console.WriteLine("Number is odd : " + numOne);
             }
         }
     }
